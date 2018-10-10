1d6ff0303e0550418ca564cbe2bb9cdf52694381
guobinfen <991402968@qq.com>
2018-10-10 11:43:26 +0800

    完成首页轮播图和右侧的的渲染

--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+//导入index组建
+import index from './components/index.vue'
+
+//导入element ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+//路由相关
+//1.导入路由模块
+import VueRouter from 'vue-router';
+//use一下
+Vue.use(VueRouter);
+//路由规则
+const routes=[
+  {
+    path:'/',
+   
+    redirect:'/index'
+
+  },
+  {
+    path:'/index',
+    component:index
+
+  }
+]
+
+//实例化路由对象
+const router=new VueRouter({
+  routes
+})
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
